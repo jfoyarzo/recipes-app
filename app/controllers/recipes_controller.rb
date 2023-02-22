f7811becfda1c4209116e0aa24d4e92667e3aa04
jfoyarzo <foyarzo621@gmail.com>
2023-02-21 22:09:02 -0300

    Update recipes controller by removing comments and unnecesary method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,24 +1,18 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :set_recipe, only: %i[show update destroy]
+  authorize_resource
 
-  # GET /recipes or /recipes.json
   def index
     user = current_user
     @recipes = user.recipes.order(created_at: :desc)
   end
 
-  # GET /recipes/1 or /recipes/1.json
   def show; end
 
-  # GET /recipes/new
   def new
     @recipe = Recipe.new
   end
 
-  # GET /recipes/1/edit
-  def edit; end
-
-  # POST /recipes or /recipes.json
   def create
     @recipe = Recipe.new(params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public))
     @recipe.user = current_user
@@ -32,7 +26,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
@@ -43,7 +36,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe.destroy
 
@@ -55,12 +47,10 @@ class RecipesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation, :time, :cooking, :time, :description, :public)
   end
