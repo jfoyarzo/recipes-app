8c34deffdeff252c3e9409b20902a6e2e78ae4f6
MattGomb <gombosonline@gmail.com>
2023-02-22 17:47:05 -0500

    Add functionality of creating a food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -20,14 +20,13 @@ class FoodsController < ApplicationController
   # POST /foods or /foods.json
   def create
     @food = Food.new(food_params)
+    @food.user = current_user
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
-        format.json { render :show, status: :created, location: @food }
+        format.html { redirect_to foods_url(@food), notice: 'Food was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -50,7 +49,7 @@ class FoodsController < ApplicationController
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.html { redirect_to foods_url, notice: 'Food was successfully deleted.' }
       format.json { head :no_content }
     end
   end
