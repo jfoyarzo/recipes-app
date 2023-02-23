808dc892746e65cb9ecaf0777fdb76360f11b11d
jfoyarzo <foyarzo621@gmail.com>
2023-02-23 17:36:59 -0300

    Update Recipe model with sum_ingredients class method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,19 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   scope :recent_recipes, -> { Recipe.where(public: true).last(5) }
+
+  def self.sum_ingredients(user_id)
+    recipes = User.find(user_id).recipes
+    ingredients = {}
+    recipes.each do |recipe|
+      recipe.recipe_foods.each do |ingredient|
+        if ingredients.key?(ingredient.food.name)
+          ingredients[ingredient.food.name] += ingredient.quantity
+        else
+          ingredients[ingredient.food.name] = ingredient.quantity
+        end
+      end
+    end
+    ingredients
+  end
 end
