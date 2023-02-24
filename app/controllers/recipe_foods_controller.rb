2988ceebe180115dec51b389b1ca945552e71b0c
MattGomb <gombosonline@gmail.com>
2023-02-24 17:58:02 -0500

    Add f, new ingredients can be added to recipes

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -9,12 +9,16 @@ class RecipeFoodsController < ApplicationController
 
   # POST /recipe_foods or /recipe_foods.json
   def create
+    unless current_user.foods.exists?(name: params[:recipe_food][:food])
+      current_user.foods.create!(name: params[:recipe_food][:food], measurement_unit: params[:recipe_food][:measurement_unit], price: params[:recipe_food][:price], quantity: 0)
+    end
+
     @recipe_food = RecipeFood.new(quantity: params[:recipe_food][:quantity])
     @recipe_food.food = Food.find_by(name: params[:recipe_food][:food])
-    @recipe_food.recipe = Recipe.find(params[:recipe_food][:recipe_id])
+    @recipe_food.recipe = Recipe.find(params[:recipe_food][:recipe_id])      
 
     respond_to do |format|
-      if @recipe_food.save
+      if @recipe_food.save!
         format.html do
           redirect_to recipe_url(@recipe_food.recipe), notice: 'The ingredient was successfully added to the recipe.'
         end
@@ -42,6 +46,6 @@ class RecipeFoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity, :food, :recipe_id)
+    params.require(:recipe_food).permit(:quantity, :food, :recipe_id, :measurement_unit, :price)
   end
 end
