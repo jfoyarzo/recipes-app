0267870ddc870c151978453a8816676a59fcedc5
MattGomb <gombosonline@gmail.com>
2023-02-24 17:59:04 -0500

    Fix linter errors

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -10,12 +10,14 @@ class RecipeFoodsController < ApplicationController
   # POST /recipe_foods or /recipe_foods.json
   def create
     unless current_user.foods.exists?(name: params[:recipe_food][:food])
-      current_user.foods.create!(name: params[:recipe_food][:food], measurement_unit: params[:recipe_food][:measurement_unit], price: params[:recipe_food][:price], quantity: 0)
+      current_user.foods.create!(name: params[:recipe_food][:food],
+                                  measurement_unit: params[:recipe_food][:measurement_unit],
+                                  price: params[:recipe_food][:price], quantity: 0)
     end
 
     @recipe_food = RecipeFood.new(quantity: params[:recipe_food][:quantity])
     @recipe_food.food = Food.find_by(name: params[:recipe_food][:food])
-    @recipe_food.recipe = Recipe.find(params[:recipe_food][:recipe_id])      
+    @recipe_food.recipe = Recipe.find(params[:recipe_food][:recipe_id])
 
     respond_to do |format|
       if @recipe_food.save!
