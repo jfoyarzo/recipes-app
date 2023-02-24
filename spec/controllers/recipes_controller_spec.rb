84f178ccfe2cda333d6b37d9661ac2793b1b3749
jfoyarzo <foyarzo621@gmail.com>
2023-02-24 13:55:23 -0300

    Add tests for legged in user

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
-  describe 'GET #index' do
-    it 'returns a success response' do
-      sign_in user(:matt)
+  describe 'Without logged in user' do
+    describe 'GET #index' do
+      it 'returns a redirect response when not logged in' do
+        get :index
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+  describe 'With user logged in' do
+    login_user
+    it 'returns a succes response when logged in' do
       get :index
-      expect(response).to be_successful
+      expect(response).to have_http_status(200)
     end
   end
 end
